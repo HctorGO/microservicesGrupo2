7b017b3768dc2772779aca5afa825127fe5c5eb6
hector.gonzalez@indexpro.com.mx <hector.gonzalez@indexpro.com.mx>
2021-01-23 11:50:19 -0600

    update practica 17

--- 17-Spring-Boot-Admin-Client/src/main/java/mx/org/certificatic/springboot/practica17/springbootadminclient/restcontroller/SpringBootAdminClientController.java
+++ 17-Spring-Boot-Admin-Client/src/main/java/mx/org/certificatic/springboot/practica17/springbootadminclient/restcontroller/SpringBootAdminClientController.java
@@ -1,8 +1,24 @@
 package mx.org.certificatic.springboot.practica17.springbootadminclient.restcontroller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@RestController
 public class SpringBootAdminClientController {
 
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	@GetMapping
+	public String hello() {
+		
+		log.info("hello");
+		
+		return "Hello World " + appName;
+	}
+	
 }
