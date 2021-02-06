c9e3835654d7191516f1c5c914eb8cd012f763d2
hgonzalez@spsolutions.com.mx <hgonzalez@spsolutions.com.mx>
2021-02-06 16:12:28 -0600

    update 21

---
+++ 21-Spring-Cloud-Config-Server/src/main/java/mx/org/certificatic/springboot/practica21/configserver/Application.java
@@ -0,0 +1,16 @@
+package mx.org.certificatic.springboot.practica21.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+// Habilita Config Server
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
