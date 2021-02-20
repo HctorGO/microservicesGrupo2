ea7a47e6aa11a151e7506551526af996aa7f860b
hector.gonzalez@spsolutions.com.mx <hector.gonzalez@spsolutions.com.mx>
2021-02-20 14:38:13 -0600

    practica 24

---
+++ 24-Users-Microservice/src/main/java/mx/org/certificatic/springboot/practica24/feign/usersmicroservice/client/IAgeServiceClient.java
@@ -0,0 +1,15 @@
+package mx.org.certificatic.springboot.practica24.feign.usersmicroservice.client;
+
+import java.util.Map;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Agrega Feign Client
+@FeignClient(name = "${age-microservice-name:age-microservice}", path = "${age-microservice.context-path:age-service}")
+public interface IAgeServiceClient {
+
+	// Agrega "/age" edpoint mediante Metodo GET.
+	@GetMapping("/age")
+	public Map<String, Object> age();
+}
